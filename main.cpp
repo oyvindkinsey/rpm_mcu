2d484cc88e5ee0523170cf1cc44c4e95cbcfffa6
Sean Kinsey <oyvind@kinsey.no>
2018-02-17 08:38:13 -0800

    Initial creation of repository and project

---
+++ main.cpp
@@ -0,0 +1,18 @@
+ /*
+ * rpm_mcu.cpp
+ *
+ * Created: 2/17/2018 8:31:50 AM
+ * Author : oyvin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
